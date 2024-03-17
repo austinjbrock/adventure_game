03d66872c584f40f21502f5f76807dad090964be
austin brock <austinjbrock@gmail.com>
2024-03-17 14:54:44 -0700

    added game play interaction

--- game.py
+++ game.py
@@ -2,16 +2,38 @@
 import time
 #start intro and welcome message
 land = 'Witwarrth'
-print('Welcome to the game, your adventure awaits.')
-print('What is your name?')
-name = input()
-print('Enter your age')
-age = int(input())
+print('Welcome to the game')
+name = input('What is your name? ')
+age = int(input("What is your age? "))
+health = 80
+
+print('Hello ', name ,' you are ', age, ' years old')
+
 if(age >= 18):
-  print('Lets get started...')
+  print('You are old enough to play')
+  wants_to_play = input('Do you want to play? ').lower()
+  if(wants_to_play == "yes"):
+    print('Here we go...')
+
+  left_or_right = input('First path choice... Left or Right (left/right)? ')
+  if left_or_right == "left":
+    answer = input("You follow the left path and stumble upon a sunny meadow... Do you continue walking or stop to rest? (walk/rest)")
+    
+    if answer == "walk":
+      health == health - 10
+      print('your health has been reduced by 10.')
+      print('your health is now :' + str(health))
+    elif answer == "rest":
+      health == health + 20
+      print('You regained your health to 100%')
+
+  else:
+    answer = input("You decided to wander down the right path, and you arrive at a lagoon. Do you swim across, or walk around?(swim/walk around)")
+
+  time.sleep(2)
+  print("You are transported to an ethereal, mystical land, named " + land)
+  time.sleep(2)
+  print('Here in ' + land, 'your fate is determined by your choice of direction... choose wisely.')
+
 else:
   print('You are not old enough to play')
-time.sleep(2)
-print("You are transported to an ethereal, mystical land, named " + land)
-time.sleep(2)
-print('Greetings ' + name + '. ' + 'Here on ' + land + ', ' + 'you have the power to control your fate.')
